980cc892c924b9790040746d4a0b6edfd08557c1
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-04-23 20:33:55 +0200

    Add louder and "page not found"

---
+++ src/pages/NotFoundPage/index.js
@@ -0,0 +1,16 @@
+let s = "!hellow !world!"
+function removeExclamationMarks(s) {
+
+}
+/*console.log(removeExclamationMarks(s));*/
+
+let arr = ["Keep", "Remove", "Keep", "Remove", "Keep"]
+function removeEveryOther(arr){
+
+}
+/*console.log( removeEveryOther(arr))*/
+
+let str = "apple ban"
+function addLength(str) {
+
+}
