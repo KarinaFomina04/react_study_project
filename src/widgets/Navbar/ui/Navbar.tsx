953bc868f87a415a2789bfd794629192df6dcb31
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-04-30 16:01:16 +0200

    Add storybook

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -11,8 +11,12 @@ interface NavbarProps {
 const Navbar = ({ className }: NavbarProps) => (
     <div className={classNames(cls.Navbar, {}, [className])}>
         <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>Главная</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">О сайте</AppLink>
+            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+                Главная
+            </AppLink>
+            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+                О сайте
+            </AppLink>
         </div>
     </div>
 );
