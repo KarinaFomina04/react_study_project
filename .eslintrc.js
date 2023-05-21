4b36af96c99d65154a3c1066f5c438294452883b
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-05-21 15:24:56 +0200

    upgrade tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,7 +46,7 @@ module.exports = {
         __IS_DEV__: true,
     },
     overrides: [{
-        files: ['**/src/**/*.test.{ts,tsx}'],
+        files: ['**/src/**/*.tests.{ts,tsx}'],
         rules: {
             'i18next/no-literal-string': 'off'
         },
