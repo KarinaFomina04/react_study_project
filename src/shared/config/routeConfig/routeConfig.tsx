5a6e95d18087e314111518c6520ccba3b7b4c4cb
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-05-21 16:11:25 +0200

    upgrade tests

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -18,14 +18,14 @@ export const RoutePath : Record<AppRoutes, string> = {
 export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.MAIN]: {
         path: RoutePath.main,
-        element: <MainPage />,
+        element: MainPage,
     },
     [AppRoutes.ABOUT]: {
         path: RoutePath.about,
-        element: <AboutPage />,
+        element: AboutPage,
     },
     [AppRoutes.NOT_FOUND]: {
         path: RoutePath.not_found,
-        element: <NotFoundPage />,
+        element: NotFoundPage,
     },
 };
