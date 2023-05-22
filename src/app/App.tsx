38a050a5ae2300a86a092016141caa71b8c1e736
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-05-22 11:34:54 +0200

    add modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,24 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProviders';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 const App = () => {
     const { theme } = useTheme();
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className={classNames('app', {}, [theme])}>
+
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={() => setIsOpen(true)}>toggle</button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} />
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
@@ -20,4 +27,5 @@ const App = () => {
         </div>
     );
 };
+
 export default App;
